82ab6bd785eaa0d72b4b41816a54c363cb438f59
Alfendio Alif Faudisyah <alfendioaliff@gmail.com>
2022-12-14 14:32:33 +0700

    update file1

--- Folder 1/file1.py
+++ Folder 1/file1.py
@@ -6,3 +6,12 @@ alas = int(input("Masukkan alas: "))
 tinggi = int(input("Masukkan tinggi: "))
 luas = alas * tinggi / 2
 print("Luas segitiga: ", luas)
+
+# membuat segitiga sama kaki
+# input: alas dan tinggi
+# output: luas segitiga
+
+alas = int(input("Masukkan alas: "))
+tinggi = int(input("Masukkan tinggi: "))
+luas = alas * tinggi / 2
+print("Luas segitiga: ", luas)
